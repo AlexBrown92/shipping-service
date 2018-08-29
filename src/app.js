6969841f5863767d64518b2ddb462830822f387c
Demo User 13 <demouser13@lab-cph.ibm.com>
2018-08-29 17:03:36 +0200

    AK - Created Service etc.

---
+++ src/app.js
@@ -0,0 +1,21 @@
+//app.js
+
+const express = require('express')
+const app = express()
+const ShippingController = require('../src/controllers/shipping-controller')
+
+let PORT = process.env.PORT || 3001
+
+app.get('/shipping', (request, response) => { 
+    let ctrl = new ShippingController()
+
+    ctrl
+        .getItemShipping({id: request.query.itemId, type: request.query.type})
+        .then(amount => {
+            response.send({ itemId: request.query.itemId, priceUSD: amount})
+        })
+        .catch(error => {
+            response.status(500).send({error: error.message })
+        })
+})
+app.listen(PORT, () => console.log(`ShippingService is lisenting on port ${PORT}`))
--- src/services/product-service.js
+++ src/services/product-service.js
@@ -1,10 +1,12 @@
 // src/services/product-service.js
 var axios = require('axios')
 
+let URL = process.env.MICROS_PRODUCTS_URL || 'mycluster.icp:8899/products'
+
 module.exports = {
     getProductWeight: async function (productId) {
         return axios
-            .get('https://mycluster.icp:8899/products/'+productId)
+            .get(`https://${URL}/${productId}`)
             .then(response => {
                 if (response.data && !Number.isNaN(parseFloat(response.data.weightLB))) {
                     return response.data.weightLB
