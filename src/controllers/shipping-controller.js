b06442bbe55afa3f3f11630575616c2d5689ac10
Demo User 13 <demouser13@lab-cph.ibm.com>
2018-08-29 12:23:34 +0200

    AK - 1st Auto Test Created

---
+++ src/controllers/shipping-controller.js
@@ -0,0 +1,7 @@
+function ShippingController() {}
+
+ShippingController.prototype.getItemShipping = function (item){
+
+}
+
+module.exports = ShippingController
--- tests/shipping-controller.test.js
+++ tests/shipping-controller.test.js
@@ -1,4 +1,14 @@
+//Declare Variables
+var chai = require ('chai')
+var expect = chai.expect
+var ShippingController = require('../src/controllers/shipping-controller')
+
 // File tests/shipping-controller.test.js
 describe('Shipping Controller', function() {
-    //TODO
+    var shippingCtrl = new ShippingController()
+
+    it('Should calculate correct shipping ', async function(){
+        let shipping = await shippingCtrl.getItemShipping({ id: 1, type: 'standard'})
+        expect(shipping).to.equal(0.5)
+    })
 })
